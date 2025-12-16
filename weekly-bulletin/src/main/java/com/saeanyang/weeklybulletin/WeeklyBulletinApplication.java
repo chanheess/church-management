b6de5cbab566ec6c6371128634b0eeaeae39b7ab
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-12-16 11:39:00 +0900

    refactor: 패키지명 수정

--- weekly-bulletin/src/main/java/com/saeanyang/weeklybulletin/WeeklyBulletinApplication.java
+++ weekly-bulletin/src/main/java/com/saeanyang/weeklybulletin/WeeklyBulletinApplication.java
@@ -1,4 +1,4 @@
-package com.saeanyang.weekly_bulletin;
+package com.saeanyang.weeklybulletin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
