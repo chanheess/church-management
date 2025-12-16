133ef3424cd27f687a6996dda1276457c0dbbda7
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2025-12-16 11:28:51 +0900

    feat: project upload

---
+++ weekly-bulletin/src/main/java/com/saeanyang/weekly_bulletin/WeeklyBulletinApplication.java
@@ -0,0 +1,13 @@
+package com.saeanyang.weekly_bulletin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyBulletinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyBulletinApplication.class, args);
+	}
+
+}
---
+++ weekly-bulletin/src/test/java/com/saeanyang/weekly_bulletin/WeeklyBulletinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saeanyang.weekly_bulletin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyBulletinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
